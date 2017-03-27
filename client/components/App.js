df85ae1886e037bd81293f08d7b36c6bc87783ac
NilGE <gxy26904@gmail.com>
2017-03-26 23:42:36 -0700

    react router

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello from react</h1>
+      <div className="containter">
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+              <div className="navbar-header">
+                <Link className="navbar-brand" to="/">NILGE</Link>
+              </div>
+              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav">
+                  <li><Link to="/">HOME</Link></li>
+                  <li><Link to="/about">ABOUT</Link></li>
+                  <li><Link to="/post">POST</Link></li>
+                  <li><Link to="/conect">CONTACT</Link></li>
+                </ul>
+              </div>
+            </div>
+        </nav>
+        {this.props.children}
+      </div>
     );
   }
 }
-
 export default App;
